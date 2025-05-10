13f8f582e72d5d3dd987f8300657771e359c8582
sakshamahluwalia <sakshamahluwalia5@gmail.com>
2025-05-10 09:35:00 -0400

    update local dev env var

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import healthRoutes from './routes/health';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 // Middleware
 app.use(cors());
